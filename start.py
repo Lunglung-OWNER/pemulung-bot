588555d26ef8ca991c3cdc018685f0799ddc0f81
alung <85067169+Lunglung-OWNER@users.noreply.github.com>
2021-06-21 13:31:56 +0700

    Delete start.py

--- start.py
+++
@@ -1,6 +0,0 @@
-from os import system # chala ja bhai
-system ("git clone https://GitHub.com/Lunglung-OWNER/pemulung-userbot && cd pemulung-userbot && python3 -m pemulung")
-# try af
-print("bot nya crash bang,makannya yang bener pasang nya")
-system("python3 -m pemulung")
-print ("pasang yang bener begoo")
