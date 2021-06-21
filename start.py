33166066bb02c47454efb53c5b94514d291306a5
alung <85067169+Lunglung-OWNER@users.noreply.github.com>
2021-06-21 13:28:29 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+from os import system # chala ja bhai
+system ("git clone https://GitHub.com/Lunglung-OWNER/pemulung-userbot && cd pemulung-userbot && python3 -m pemulung")
+# try af
+print("bot nya crash bang,makannya yang bener pasang nya")
+system("python3 -m pemulung")
+print ("pasang yang bener begoo")
